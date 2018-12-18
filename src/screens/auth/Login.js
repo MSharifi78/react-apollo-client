96a8103b3fe515b3f47bc18eb617f1afa1973900
Mohammad Sharifi <techshow78@gmail.com>
2018-12-18 22:43:18 +0330

    add Screens

---
+++ src/screens/auth/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import {} from "react-router-dom";
+
+export class Login extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default Login;
