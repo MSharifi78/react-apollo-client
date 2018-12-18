86204c41d615c89e02c21b4ff0557c806e5109b5
Mohammad Sharifi <techshow78@gmail.com>
2018-12-18 23:46:22 +0330

    add auth routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./screens/auth/Login";
+import Register from "./screens/auth/Register";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Router>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Router>
+    );
   }
 }
 
