cd358174f84d2be6a90c7f6ef408c4bacc5a4acb
Mohammad Sharifi <techshow78@gmail.com>
2019-01-04 17:52:22 +0330

    create apollo client

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Login from "./screens/auth/Login";
 import Register from "./screens/auth/Register";
 import Home from "./screens/app/Home";
 import PrivateRoute from "./components/PrivateRoute";
+import {ApolloProvider} from "react-apollo"
+import client from './apollo/client'
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <Router>
         <Switch>
           <Route path="/login" component={Login} />
@@ -15,6 +18,7 @@ class App extends Component {
           <PrivateRoute path="/" component={Home} redirectPath="/login" />
         </Switch>
       </Router>
+      </ApolloProvider>
     );
   }
 }
