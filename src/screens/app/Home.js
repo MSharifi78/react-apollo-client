b4ad594c88b8f70e5e25773c29999fab0baeca5b
Mohammad Sharifi <techshow78@gmail.com>
2019-01-04 17:50:20 +0330

    Change Home UI

--- src/screens/app/Home.js
+++ src/screens/app/Home.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
+import { Grid, Header } from "semantic-ui-react";
 
 export class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <Grid
+        style={{ height: "100%", backgroundColor: "#fafafa" }}
+        verticalAlign="middle"
+        centered
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h1" color="orange" textAlign="center">
+            YOU DONE IT!!!
+          </Header>
+        </Grid.Column>
+      </Grid>
+    )
   }
 }
 
